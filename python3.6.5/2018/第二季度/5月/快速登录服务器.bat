9ca52fba0d6e5c684ebf650bee8885892a41d55c
fblrainbow <846058904@qq.com>
2018-08-13 00:22:38 +0800

    [Time]    : [commit]  :通过配置文件进行登录 [review]  :fanbinglin

--- python3.6.5/2018/第二季度/5月/快速登录服务器.bat
+++ python3.6.5/2018/第二季度/5月/快速登录服务器.bat
@@ -1,2 +1,9 @@
-﻿@echo off
-putty root@www.fanbinglin.com -pw FBLATPX4869@
+@echo off
+setlocal enabledelayedexpansion
+for /F "eol=; tokens=1,2 delims==" %%i in (config.ini) do (
+	if %%i==IP set IP=%%j
+	if %%i==Account set Account=%%j
+	if %%i==Password set Password=%%j
+)
+set string=%Account%@%IP% -pw %Password%
+start putty %string%

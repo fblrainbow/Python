19a2341e9dd4b172b6ca1a9dde029e2277a2e3b6
fblrainbow <846058904@qq.com>
2018-09-19 00:52:17 +0800

    [Time]    :2018-8-13 00:23:18 [commit]  : [review]  :fanbinglin

--- python3.6.5/5.9/python发邮件/except.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-#coding:utf-8
-import os
-import sys
-# try:
-cmd = 'sdfaf'
-os.system(cme)
-# except:
-    # print('Error:无法发送邮件')
--- python3.6.5/5.9/python发邮件/启动.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d D:\SVN\Python\trunk\python3.6.5\5.9\python发邮件\
-cd /d d:\SVN\Python\trunk\python3.6.5\5.9\python发邮件
-
-
-:
-ping 47.91.232.233 -n 3 >test.txt
-for /f "skip=8 takens=2,* delims== " %%i in (test.txt) do @echo %%i

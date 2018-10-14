21d8665a40717a0fac1599e1878d73f027cc5164
fblrainbow <846058904@qq.com>
2018-10-14 23:15:27 +0800

    [commit]  :显示特效 [review]  :fanbinglin

--- python3.6.5/PyQt5/UI.py
+++ python3.6.5/PyQt5/UI.py
@@ -8,20 +8,42 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_closeWinBtn(object):
-    def setupUi(self, closeWinBtn):
-        closeWinBtn.setObjectName("closeWinBtn")
-        closeWinBtn.resize(400, 300)
-        self.pushButton = QtWidgets.QPushButton(closeWinBtn)
-        self.pushButton.setGeometry(QtCore.QRect(140, 80, 75, 23))
-        self.pushButton.setObjectName("pushButton")
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(566, 258)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox.setGeometry(QtCore.QRect(130, 40, 71, 16))
+        self.checkBox.setObjectName("checkBox")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(100, 120, 54, 12))
+        self.label.setObjectName("label")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(190, 110, 113, 20))
+        self.lineEdit.setObjectName("lineEdit")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.toolBar = QtWidgets.QToolBar(MainWindow)
+        self.toolBar.setObjectName("toolBar")
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+        self.menuBar = QtWidgets.QMenuBar(MainWindow)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 566, 23))
+        self.menuBar.setObjectName("menuBar")
+        MainWindow.setMenuBar(self.menuBar)
 
-        self.retranslateUi(closeWinBtn)
-        self.pushButton.clicked.connect(closeWinBtn.close)
-        QtCore.QMetaObject.connectSlotsByName(closeWinBtn)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, closeWinBtn):
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        closeWinBtn.setWindowTitle(_translate("closeWinBtn", "Form"))
-        self.pushButton.setText(_translate("closeWinBtn", "关闭窗口"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "QQ"))
+        MainWindow.setToolTip(_translate("MainWindow", "<html><head/><body><p><a href=\"https://www.baidu.com\"><span style=\" text-decoration: underline; color:#0000ff;\">百度</span></a></p></body></html>"))
+        self.checkBox.setText(_translate("MainWindow", "选择"))
+        self.label.setText(_translate("MainWindow", "显示1"))
+        self.lineEdit.setText(_translate("MainWindow", "显示2"))
+        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
 
--- python3.6.5/PyQt5/execution.py
+++ python3.6.5/PyQt5/execution.py
@@ -8,11 +8,11 @@
 
 #from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QMainWindow, QApplication
-from UI import Ui_closeWinBtn
+from UI import Ui_MainWindow
 
-class Ui(QMainWindow, Ui_closeWinBtn):
+class Ui(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
-        super(Ui_closeWinBtn, self).__init__()
+        super(Ui_MainWindow, self).__init__()
         #self.setupUi(self)
 
 

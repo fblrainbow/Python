14c6255db9725f4362ebf5081d6f66a89bf64b1f
fblrainbow <846058904@qq.com>
2018-11-04 04:56:05 +0800

    [commit]  :界面删除复选框，脚本下拉框实现打开过滤的文件，关闭窗口 [review]  :fanbinglin

--- python3.6.5/PyQt5/execution.py
+++ python3.6.5/PyQt5/execution.py
@@ -7,15 +7,21 @@
 # WARNING! All changes made in this file will be lost!
 
 #from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QFileDialog
 from selectDisplay import Ui_MainWindow
 
 class Ui(QMainWindow, Ui_MainWindow):
-    def __init__(self, parent=None):
-        super(Ui_MainWindow, self).__init__()
+    def __init__(self):
+        super(Ui, self).__init__()
         super(QMainWindow, self).__init__()
         self.setupUi(self)
-
+        print(dir(self))
+        #菜单点击关闭时，连接槽函数close()
+        self.Close.triggered.connect(self.close)
+        self.Open.triggered.connect(self.openMsg)
+    def openMsg(self):
+        file, ok = QFileDialog.getOpenFileName(self, "打开", "F:/", "ALL dffsfs Files (*);;Text Files (*.txt)")
+        self.statusbar.showMessage(file)
 
 if __name__ == "__main__":
     import sys

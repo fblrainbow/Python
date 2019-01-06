525604e756e1107183ae8fcc4d6dd611ea0f34ef
fblrainbow <846058904@qq.com>
2019-01-06 23:19:33 +0800

    [commit]  :陆强的牛筋草抗药性水平查询系统 [review]  :fanbinglin

--- python3.6.5/luqiang/LuQiang/main-byname.py
+++ python3.6.5/luqiang/LuQiang/main-byname.py
@@ -15,8 +15,8 @@ class MyWindow(QWidget):
         super(MyWindow, self).__init__(parent)
         self.workList = openWorkBook("csj.xlsx")
         #self.setLayout(layout)
-        self.setWindowTitle("种群数据搜索")
-        self.resize(700,200)
+        self.setWindowTitle("牛筋草抗药性水平查询系统")
+        self.resize(1280,200)
         #全局控件，用于“承载”全局变量
         #wwg = QWidget(self)
         #全局布局（注意参数wwg）
@@ -34,55 +34,117 @@ class MyWindow(QWidget):
         self.search.setMaximumWidth(40)
         self.hlayout_Search.addWidget(self.search,1,Qt.AlignLeft)
         self.search.clicked.connect(self.getSearchText)
-        self.tmodel = QStandardItemModel(3,18)
-        self.tmodel.setHorizontalHeaderLabels(['种群','穗分枝数(个)','分支籽粒充实度(%)','a','b','R2','GR50(g a.i.ha-1)','95%置信区间(GR50)','IR','a','b','R2','GR50(g a.i.ha-1)','95%置信区间(GR50)','IR','采样地点','生境'])
+        self.gtTabgwt = QTableWidget(self)
+        self.gtTabgwt.setRowCount(3)
+        self.gtTabgwt.setColumnCount(17)
+        self.gtTabgwt.setHorizontalHeaderLabels(['种群','穗分枝数(个)','分支籽粒充实度(%)','a','b','R2','GR50(g a.i.ha-1)','95%置信区间(GR50)','IR','a','b','R2','GR50(g a.i.ha-1)','95%置信区间(GR50)','IR','采样地点','生境'])
         #self.tmodel.setHorizontalHeaderLabels(
         
         #['种群','穗分枝数(个)','分支籽粒充实度(%)','a','b','R2','GR50(g a.i.ha-1)','95%置信区间(GR50)','IR','a','b','R2','GR50(g a.i.ha-1)','95%置信区间(GR50)','IR','采样地点','生境'])
+        """
         self.tableView = QTableView(self)
         self.tableView.setModel(self.tmodel)
         self.tableView.setShowGrid(True)
-        self.hlayout_table.addWidget(self.tableView)
+        """
 
+        self.hlayout_table.addWidget(self.gtTabgwt)
+ 
         self.hwg_s = QWidget(self)
         self.hwg_t = QWidget(self)
+        #self.gtTabgwt.setColumnHidden(0,True)
+        self.gtTabgwt.verticalHeader().setVisible(False)
+
+        self.gtTabgwt.horizontalHeader().setVisible(False)
         
         self.hwg_s.setLayout(self.hlayout_Search)
         self.hwg_t.setLayout(self.hlayout_table)
         
         
+        level_1 = ['种群','穗分枝数(个)','分支籽粒充实度(%)','a','b','R2','GR50(g a.i.ha-1)','95%置信区间(GR50)','IR','a','b','R2','GR50(g a.i.ha-1)','95%置信区间(GR50)','IR','采样地点','生境']
+        
+        self.gtTabgwt.setSpan(0,0,2,1)
+        self.gtTabgwt.setSpan(0,1,1,2)
+        #self.gtTabgwt.setSpan(0,2,2,1)
+        self.gtTabgwt.setSpan(0,3,1,6)
+        #self.gtTabgwt.setSpan(0,4,2,1)
+        #self.gtTabgwt.setSpan(0,5,2,1)
+        #self.gtTabgwt.setSpan(0,6,2,1)
+        #self.gtTabgwt.setSpan(0,7,2,1)
+        #self.gtTabgwt.setSpan(0,8,2,1)
+        self.gtTabgwt.setSpan(0,9,1,6)
+        #self.gtTabgwt.setSpan(0,10,2,1)
+        #self.gtTabgwt.setSpan(0,11,2,1)
+        #self.gtTabgwt.setSpan(0,12,2,1)
+        #self.gtTabgwt.setSpan(0,13,2,1)
+        #self.gtTabgwt.setSpan(0,14,2,1)
+        self.gtTabgwt.setSpan(0,15,1,2)
+        #self.gtTabgwt.setSpan(0,16,2,1)
+        self.gtTabgwt.setRowHeight(0,10)
+        self.gtTabgwt.setRowHeight(1,10)
+        
+        self.gtTabgwt.setItem(0,0,QTableWidgetItem("种群"))
+        self.gtTabgwt.setItem(0,1,QTableWidgetItem("形态指标"))
+        self.gtTabgwt.setItem(1,1,QTableWidgetItem("穗分枝数(个)"))
+        self.gtTabgwt.setItem(1,2,QTableWidgetItem("分支籽粒充实度(%)"))
+        self.gtTabgwt.setItem(0,3,QTableWidgetItem("对百草枯的抗药性水平"))
+        self.gtTabgwt.setItem(1,3,QTableWidgetItem("a"))
+        self.gtTabgwt.setItem(1,4,QTableWidgetItem("b"))
+        self.gtTabgwt.setItem(1,5,QTableWidgetItem("R2"))
+        self.gtTabgwt.setItem(1,6,QTableWidgetItem("GR50(g a.i.ha-1)"))
+        self.gtTabgwt.setItem(1,7,QTableWidgetItem("95%置信区间(GR50)"))
+        self.gtTabgwt.setItem(1,8,QTableWidgetItem("IR"))
+        
+        
+        self.gtTabgwt.setItem(0,9,QTableWidgetItem("对草甘膦的抗药性水平"))
+        self.gtTabgwt.setItem(1,9,QTableWidgetItem("a"))
+        self.gtTabgwt.setItem(1,10,QTableWidgetItem("b"))
+        self.gtTabgwt.setItem(1,11,QTableWidgetItem("R2"))
+        self.gtTabgwt.setItem(1,12,QTableWidgetItem("GR50(g a.i.ha-1)"))
+        self.gtTabgwt.setItem(1,13,QTableWidgetItem("95%置信区间(GR50)"))
+        self.gtTabgwt.setItem(1,14,QTableWidgetItem("IR"))
+        self.gtTabgwt.setItem(0,15,QTableWidgetItem("采集地点及用药史"))
+        self.gtTabgwt.setItem(1,15,QTableWidgetItem("采样地点"))
+        self.gtTabgwt.setItem(1,16,QTableWidgetItem("生境"))
+        self.gtTabgwt.setLineWidth(2)
+        #print(dir(self.gtTabgwt))
+        #self.gtTabgwt.setTextAlignment(Qt.AlignHCenter)
+        
+        #self.gtTabgwt.setColumnWidth(0,20)
+
+        
+        
         self.gWg.addWidget(self.hwg_s)
         self.gWg.addWidget(self.hwg_t)
         self.setLayout(self.gWg)
+        self.gtTabgwt.resizeColumnsToContents()
+        self.gtTabgwt.resizeRowsToContents()
+        
+        #self.gtTabgwt.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
     def getSearchText(self):
         #print(self.qle.text())
+        
         self.searchText = self.qle.text()
         self.retList = searchData(self.searchText,self.workList)
-        #print(self.retList)
+        print(self.retList)
+        for index in range(0,16,1):
+            item = QTableWidgetItem('')
+            self.gtTabgwt.setItem(2,index,item)
         if len(self.retList) == 0:
             print("没有搜索到该种群")
-            return
-        tableList = [self.retList[0][0],self.retList[2][1],self.retList[2][2],self.retList[0][1],self.retList[0][2],self.retList[0][3],self.retList[0][4],self.retList[0][5],self.retList[0][6],self.retList[1][1],self.retList[1][2],self.retList[1][3],self.retList[1][4],self.retList[1][5],self.retList[1][6],self.retList[3][1],self.retList[3][2]]
-        
-        #print(tableList)
-        print(dir(self.tmodel))
-        self.tmodel.clear()
-        for index,line in enumerate(tableList):
-            item = QStandardItem(line)
-            self.tmodel.setItem(2,index,item)
-    def getIext(self):
-        text = self.le2.text()
-        self.retList = searchData(text,self.workList)
-        print(self.retList)
-        self.model = QStandardItemModel(1,17);
-        self.model.setHorizontalHeaderLabels(['种群','穗分枝数(个)','分支籽粒充实度(%)','a','b','R2','GR50(g a.i.ha-1)','95%置信区间(GR50)','IR','a','b','R2','GR50(g a.i.ha-1)','95%置信区间(GR50)','IR','采样地点','生境'])
-        item = QStandardItem('0')
-        self.model.setItem(0,0,item)
-        self.tableView = QTableView()
-        self.tableView.setModel(self.model)
-        dlgLayout = QVBoxLayout();
-        dlgLayout.addWidget(self.tableView)
-        self.addLayout(dlgLayout)
+        else:
+            try:
+                tableList = [self.retList[0][0],self.retList[2][1],self.retList[2][2],self.retList[0][1],self.retList[0][2],self.retList[0][3],self.retList[0][4],self.retList[0][5],self.retList[0][6],self.retList[1][1],self.retList[1][2],self.retList[1][3],self.retList[1][4],self.retList[1][5],self.retList[1][6],self.retList[3][1],self.retList[3][2]]
+                #print(tableList)
+                #self.gtTabgwt.clear()
+                for index,line in enumerate(tableList):
+                    item = QTableWidgetItem(line)
+                    self.gtTabgwt.setItem(2,index,item)
+                self.gtTabgwt.resizeColumnsToContents()
+                self.gtTabgwt.resizeRowsToContents()
+                #self.gtTabgwt.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+            except:
+                pass
+            
 def openWorkBook(fileName):
     workbook = xlrd.open_workbook(fileName)
     sheetName_0 = workbook.sheet_by_name("不同种群牛筋草对百草枯的抗药性水平")
